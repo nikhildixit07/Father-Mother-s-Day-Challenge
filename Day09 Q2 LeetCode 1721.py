eb5cb6e4feb53a8d44c3592d459d98a14ad85cc2
Nikhil <hdixit07@gmail.com>
2025-06-05 19:34:46 +0530

    Create Day09 Q2 LeetCode 1721.py

---
+++ Day09 Q2 LeetCode 1721.py
@@ -0,0 +1,30 @@
+# Swapping Nodes in a Linked List (#1721)
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def swapNodes(self, head: ListNode, k: int) -> ListNode:
+        # Find the length of the linked list
+        length = 0
+        current = head
+        while current:
+            length += 1
+            current = current.next
+
+        # Find the k-th node from the beginning
+        first = head
+        for _ in range(k - 1):
+            first = first.next
+
+        # Find the k-th node from the end
+        second = head
+        for _ in range(length - k):
+            second = second.next
+
+        # Swap their values
+        first.val, second.val = second.val, first.val
+
+        return head
