0a6e10885b79e6677b8a172a22462a6be6853087
Nikhil <hdixit07@gmail.com>
2025-06-05 19:33:02 +0530

    Delete Day8 Q1 LeetCode 171.py

--- Day8 Q1 LeetCode 171.py
+++
@@ -1,7 +0,0 @@
-# Excel Sheet Column Number (#171)
-class Solution:
-    def titleToNumber(self, columnTitle: str) -> int:
-        result = 0
-        for c in columnTitle:
-            result = result * 26 + (ord(c) - ord('A') + 1)
-        return result
