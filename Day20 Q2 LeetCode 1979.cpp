99684bf4697f0b33ca4198c6199aec661a067f70
Nikhil <hdixit07@gmail.com>
2025-06-05 19:31:46 +0530

    Create Day20 Q2 LeetCode 1979.cpp

---
+++ Day20 Q2 LeetCode 1979.cpp
@@ -0,0 +1,9 @@
+// Find Greatest Common Divisor of Array (#1979)
+class Solution {
+public:
+    int findGCD(vector<int>& nums) {
+        int mini = *min_element(nums.begin(), nums.end());
+        int maxi = *max_element(nums.begin(), nums.end());
+        return __gcd(mini, maxi); // built-in GCD function
+    }
+};
