56464cbc2c9bd305ee4ed3a2f3dd53b958d04a95
Nikhil <hdixit07@gmail.com>
2025-06-06 21:35:10 +0530

    Create Day27 Q1 LeetCode 112.cpp

---
+++ Day27 Q1 LeetCode 112.cpp
@@ -0,0 +1,29 @@
+// Path Sum (#112)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        // If the tree is empty, return false
+        if (!root) return false;
+
+        // If it's a leaf node, check if the path sum equals targetSum
+        if (!root->left && !root->right) {
+            return root->val == targetSum;
+        }
+
+        // Recursively check the left and right subtree with reduced target sum
+        int remainingSum = targetSum - root->val;
+        return hasPathSum(root->left, remainingSum) || hasPathSum(root->right, remainingSum);
+    }
+};
