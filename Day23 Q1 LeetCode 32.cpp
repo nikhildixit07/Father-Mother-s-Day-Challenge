6486ae7a5290a216365137da4986c4ae672c6048
Nikhil <hdixit07@gmail.com>
2025-06-06 21:26:09 +0530

    Create Day23 Q1 LeetCode 32.cpp

---
+++ Day23 Q1 LeetCode 32.cpp
@@ -0,0 +1,34 @@
+// Longest Valid Parentheses (#32)
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int left = 0, right = 0, maxLen = 0;
+
+        // Left to right pass
+        for (char c : s) {
+            if (c == '(') left++;
+            else right++;
+
+            if (left == right) {
+                maxLen = max(maxLen, 2 * right);
+            } else if (right > left) {
+                left = right = 0;
+            }
+        }
+
+        // Right to left pass
+        left = right = 0;
+        for (int i = s.size() - 1; i >= 0; --i) {
+            if (s[i] == '(') left++;
+            else right++;
+
+            if (left == right) {
+                maxLen = max(maxLen, 2 * left);
+            } else if (left > right) {
+                left = right = 0;
+            }
+        }
+
+        return maxLen;
+    }
+};
