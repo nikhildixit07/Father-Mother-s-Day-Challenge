e47901e92b7517ba2a28fc84bbb896d40a29f9d6
Nikhil <hdixit07@gmail.com>
2025-06-06 21:26:02 +0530

    Create Day22 Q1 LeetCode 1249.py

---
+++ Day22 Q1 LeetCode 1249.py
@@ -0,0 +1,21 @@
+# Minimum Remove to Make Valid Parentheses (#1249)
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        s = list(s)
+        stack = []
+
+        # First pass: Remove extra ')'
+        for i in range(len(s)):
+            if s[i] == '(':
+                stack.append(i)
+            elif s[i] == ')':
+                if stack:
+                    stack.pop()
+                else:
+                    s[i] = ''
+
+        # Remove unmatched '('
+        while stack:
+            s[stack.pop()] = ''
+
+        return ''.join(s)
