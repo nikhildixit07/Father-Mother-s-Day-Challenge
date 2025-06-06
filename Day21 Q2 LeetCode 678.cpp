3ea3b3555568041b715636df0b838d180ac36b4c
Nikhil <hdixit07@gmail.com>
2025-06-06 21:25:59 +0530

    Create Day21 Q2 LeetCode 678.cpp

---
+++ Day21 Q2 LeetCode 678.cpp
@@ -0,0 +1,22 @@
+// Valid Parenthesis String (#678)
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int low = 0, high = 0;
+        for (char c : s) {
+            if (c == '(') {
+                low++;
+                high++;
+            } else if (c == ')') {
+                low--;
+                high--;
+            } else { // c == '*'
+                low--;     // treat '*' as ')'
+                high++;    // treat '*' as '('
+            }
+            if (high < 0) return false;  // too many ')'
+            if (low < 0) low = 0;        // '*' saved us
+        }
+        return low == 0;
+    }
+};
