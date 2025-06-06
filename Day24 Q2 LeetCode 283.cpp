fe1d3f6110ee0add832cbf7ee11a80acb8642b61
Nikhil <hdixit07@gmail.com>
2025-06-06 21:26:21 +0530

    Create Day24 Q2 LeetCode 283.cpp

---
+++ Day24 Q2 LeetCode 283.cpp
@@ -0,0 +1,19 @@
+// Move Zeroes (#283)
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int insertPos = 0;
+
+        // Move all non-zero elements to the front
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] != 0) {
+                nums[insertPos++] = nums[i];
+            }
+        }
+
+        // Fill the rest with zeroes
+        while (insertPos < nums.size()) {
+            nums[insertPos++] = 0;
+        }
+    }
+};
