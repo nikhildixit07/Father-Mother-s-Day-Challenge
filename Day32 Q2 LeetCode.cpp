cf1a7ad00b61e4914e96215f7833c78495971f1e
Nikhil <hdixit07@gmail.com>
2025-06-07 17:21:37 +0530

    Create Day32 Q2 LeetCode.cpp

---
+++ Day32 Q2 LeetCode.cpp
@@ -0,0 +1,19 @@
+// Rotate Image (#48)
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        
+        // Step 1: Transpose the matrix (convert rows to columns)
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        
+        // Step 2: Reverse each row to get the final rotated matrix
+        for (int i = 0; i < n; ++i) {
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+    }
+};
