bf5e78095413ce88393690052e81cee3d66ac2f2
Nikhil <hdixit07@gmail.com>
2025-06-07 17:13:44 +0530

    Create Day30 Q1 LeetCode 543.cpp

---
+++ Day30 Q1 LeetCode 543.cpp
@@ -0,0 +1,37 @@
+// Diameter of Binary Tree (#543)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    int maxDiameter = 0; // Stores the maximum diameter found
+
+    // Helper function to calculate height and update diameter
+    int dfs(TreeNode* root) {
+        if (!root) return 0;
+
+        // Recursively find the height of left and right subtrees
+        int leftHeight = dfs(root->left);
+        int rightHeight = dfs(root->right);
+
+        // Update the diameter at this node if the path through it is longer
+        maxDiameter = max(maxDiameter, leftHeight + rightHeight);
+
+        // Return the height of the current subtree
+        return 1 + max(leftHeight, rightHeight);
+    }
+
+    int diameterOfBinaryTree(TreeNode* root) {
+        dfs(root);
+        return maxDiameter;
+    }
+};
