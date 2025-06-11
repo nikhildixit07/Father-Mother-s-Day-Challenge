54f589ec6667666b5b3f7043d8b91cf38a0f120a
Nikhil <hdixit07@gmail.com>
2025-06-11 23:53:10 +0530

    Create Day34 Q2 LeetCode 2697.cpp

---
+++ Day34 Q2 LeetCode 2697.cpp
@@ -0,0 +1,21 @@
+// Length of Last Word (#58)
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int n = s.length();
+        int length = 0;
+        bool counting = false;
+
+        // Traverse string from end to start
+        for (int i = n - 1; i >= 0; i--) {
+            if (s[i] != ' ') {
+                counting = true;
+                length++;
+            } else if (counting) {
+                // We've counted a word, and hit a space
+                break;
+            }
+        }
+        return length;
+    }
+};
