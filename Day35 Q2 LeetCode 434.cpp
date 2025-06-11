4511ef1a12fdc47b3332a0dc9a1970833a8b19b6
Nikhil <hdixit07@gmail.com>
2025-06-11 23:53:18 +0530

    Create Day35 Q2 LeetCode 434.cpp

---
+++ Day35 Q2 LeetCode 434.cpp
@@ -0,0 +1,18 @@
+// Number of Segments in a String (#434)
+class Solution {
+public:
+    int countSegments(string s) {
+        int count = 0;
+        int n = s.length();
+
+        // Traverse through the string character by character
+        for (int i = 0; i < n; ++i) {
+            // Check if the current character is not a space
+            // and either it's the first character or the previous one was a space
+            if (s[i] != ' ' && (i == 0 || s[i - 1] == ' ')) {
+                count++; // Start of a new segment
+            }
+        }
+        return count;
+    }
+};
