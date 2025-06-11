5269f59c3b4e5788ac8b208055a4af59347b6787
Nikhil <hdixit07@gmail.com>
2025-06-11 23:53:04 +0530

    Create Day33 Q2 LeetCode 2697.cpp

---
+++ Day33 Q2 LeetCode 2697.cpp
@@ -0,0 +1,20 @@
+// Lexicographically Smallest Palindrome (#2697)
+class Solution {
+public:
+    string makeSmallestPalindrome(string s) {
+        int left = 0;
+        int right = s.size() - 1;
+
+        // Use two-pointer approach to check and modify the string from both ends
+        while (left < right) {
+            // If characters don't match, replace the larger one with the smaller one
+            if (s[left] != s[right]) {
+                char smallerChar = min(s[left], s[right]);
+                s[left] = s[right] = smallerChar;
+            }
+            left++;
+            right--;
+        }
+        return s;
+    }
+};
