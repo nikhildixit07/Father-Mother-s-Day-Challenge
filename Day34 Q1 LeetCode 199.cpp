bd961abc0c2a74153bb9f32f6bf16b9ffdbbebd2
Nikhil <hdixit07@gmail.com>
2025-06-11 23:53:08 +0530

    Create Day34 Q1 LeetCode 199.cpp

---
+++ Day34 Q1 LeetCode 199.cpp
@@ -0,0 +1,29 @@
+// Flatten Binary Tree to Linked List (#114)
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        if (!root) return;
+
+        std::stack<TreeNode*> st;
+        st.push(root);
+        TreeNode* prev = nullptr;
+
+        while (!st.empty()) {
+            TreeNode* curr = st.top();
+            st.pop();
+
+            // If there's a previous node, point its right to current
+            if (prev) {
+                prev->left = nullptr;    // Left should always be null
+                prev->right = curr;
+            }
+
+            // Since stack is LIFO, push right first so left is processed first
+            if (curr->right) st.push(curr->right);
+            if (curr->left) st.push(curr->left);
+
+            // Move to the next node
+            prev = curr;
+        }
+    }
+};
