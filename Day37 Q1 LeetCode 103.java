0c2d5af146cd2a29c5ff5824e6a58257ecb92aab
Nikhil <hdixit07@gmail.com>
2025-06-12 23:07:29 +0530

    Create Day37 Q1 LeetCode 103.java

---
+++ Day37 Q1 LeetCode 103.java
@@ -0,0 +1,40 @@
+// Binary Tree Zigzag Level Order Traversal (#103)
+public class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        
+        boolean leftToRight = true;
+        
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>();
+            
+            // Collect nodes normally
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+            }
+            
+            // Reverse the level list if direction is right-to-left
+            if (!leftToRight) {
+                Collections.reverse(level);
+            }
+            result.add(level);
+            leftToRight = !leftToRight;
+        }
+        return result;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int val) { this.val = val; }
+}
