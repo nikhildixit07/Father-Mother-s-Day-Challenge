18a7e69adc85b86817f1663181bd6072d5d86e8c
Nikhil <hdixit07@gmail.com>
2025-06-15 23:07:00 +0530

    Create Day41 Q2 LeetCode 14.java

---
+++ Day41 Q2 LeetCode 14.java
@@ -0,0 +1,18 @@
+// Longest Common Prefix (#14)
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+
+        String prefix = strs[0];
+
+        for (int i = 1; i < strs.length; i++) {
+            // Keep trimming the prefix until it matches
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "";
+            }
+        }
+        return prefix;
+    }
+}
