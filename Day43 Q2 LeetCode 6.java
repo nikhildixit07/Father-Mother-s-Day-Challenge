d50d3fa9335104a2d062db2b97229ae762364de6
Nikhil <hdixit07@gmail.com>
2025-06-15 23:07:11 +0530

    Create Day43 Q2 LeetCode 6.java

---
+++ Day43 Q2 LeetCode 6.java
@@ -0,0 +1,28 @@
+// Zigzag Conversion (#6)
+
+class Solution {
+    public String convert(String s, int numRows) {
+        if (numRows == 1 || s.length() <= numRows) {
+            return s;
+        }
+        // Create StringBuilder for each row
+        StringBuilder[] rows = new StringBuilder[numRows];
+        for (int i = 0; i < numRows; i++) {
+            rows[i] = new StringBuilder();
+        }
+        int row = 0;
+        boolean goingDown = false;
+        for (char c : s.toCharArray()) {
+            rows[row].append(c);
+            if (row == 0 || row == numRows - 1) {
+                goingDown = !goingDown;
+            }
+            row += goingDown ? 1 : -1;
+        }
+        StringBuilder result = new StringBuilder();
+        for (StringBuilder sb : rows) {
+            result.append(sb);
+        }
+        return result.toString();
+    }
+}
