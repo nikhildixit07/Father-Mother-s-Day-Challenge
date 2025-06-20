626e45d78ee64e9efd94343a8ac48703952494e7
Nikhil <hdixit07@gmail.com>
2025-06-21 03:45:06 +0530

    Create Day49 Q2 LeetCode 136.cpp

---
+++ Day49 Q2 LeetCode 136.cpp
@@ -0,0 +1,12 @@
+// Single Number (#136)
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
+    }
+};
